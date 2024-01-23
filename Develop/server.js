59b1987d9ade9b18850dc7e4dba9b996a5ed444d
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-01-23 13:29:16 -0500

    got npm start working

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
