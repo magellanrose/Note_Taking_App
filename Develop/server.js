52ad2189e147f4ffe9cb0ccb5aaa89ce8c50cc3a
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-01-23 11:28:36 -0500

    fixed file path in server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -10,14 +10,17 @@ app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
 
 // Route for the notes.html file
-app.get('/send', (req, res) =>
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 // Route for the index.html file
-app.get('/paths', (req, res) =>
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
+
+
+
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
